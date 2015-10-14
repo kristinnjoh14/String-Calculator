09f09ab908d91d003712209d027da3add385dfba
Kristinn Jóhannsson <kiddinn95@gmail.com>
2015-10-14 14:58:47 +0000

    Skref 2

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import com.sun.xml.internal.fastinfoset.util.CharArray;
+
 import java.util.IntSummaryStatistics;
 
 public class Main {
@@ -11,17 +13,26 @@ public class Main {
             sum = Integer.parseInt(numbers);
         }
         else if (numbers.contains(",")) {
-            int index = numbers.indexOf(',');
-            int num1 = Integer.parseInt(numbers.substring(0,index));
-            int num2 = Integer.parseInt(numbers.substring(index + 1));
-            sum = num1 + num2;
-            //System.out.print(index);
+            int index1 = 0;
+            StringBuffer nums = new StringBuffer(numbers);
+            while (nums.toString().contains(",") || index1 == -1) {
+                int index = nums.toString().indexOf(',');
+                int num1 = Integer.parseInt(nums.substring(index1, index));
+                //int num2 = Integer.parseInt(numbers.substring(index + 1));
+                sum = sum + num1;
+                //System.out.print(index);
+                nums = nums.deleteCharAt(index);
+                index1 = index;
+                //System.out.print(nums.charAt(index));
+            }
+            int num1 = Integer.parseInt(nums.substring(index1));
+            sum = sum + num1;
         }
         return sum;
     }
 
     public static void main(String[] args) {
-        String numbers = "2,3";
+        String numbers = "1,25,3,40";
         System.out.print(Add(numbers));
     }
 }
