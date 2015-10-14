be4eb1edee7a97fe5874ad6f6497040883524ee6
Kristinn Jóhannsson <kiddinn95@gmail.com>
2015-10-14 15:36:38 +0000

    Skref 3

--- Main.java
+++ Main.java
@@ -9,14 +9,20 @@ public class Main {
     public static int Add(String numbers) {
         int sum = 0;
         if(numbers.length() == 0) {}
-        else if (!numbers.contains(",")) {
+        else if (!numbers.contains(",") && !numbers.contains("/n")) {
             sum = Integer.parseInt(numbers);
         }
-        else if (numbers.contains(",")) {
+        else if (numbers.contains(",") || numbers.contains(("/n"))) {
             int index1 = 0;
             StringBuffer nums = new StringBuffer(numbers);
-            while (nums.toString().contains(",") || index1 == -1) {
+            while (nums.toString().contains(",") || nums.toString().contains("/n") || index1 == -1) {
                 int index = nums.toString().indexOf(',');
+                int nindex = nums.toString().indexOf('/');
+                if ((nindex < index || index == -1) && nindex != -1) {
+                    index = nindex;
+                    nums.deleteCharAt(index);
+                }
+
                 int num1 = Integer.parseInt(nums.substring(index1, index));
                 //int num2 = Integer.parseInt(numbers.substring(index + 1));
                 sum = sum + num1;
@@ -32,7 +38,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        String numbers = "1,25,3,40";
+        String numbers = "1,25/n3,7/n5,4/n5";
         System.out.print(Add(numbers));
     }
 }
