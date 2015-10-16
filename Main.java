159f269f47d84f96163d96e200aded547a7d594e
Kiddi <kiddinn95@gmail.com>
2015-10-16 00:31:48 +0000

    Skref 4

--- Main.java
+++ Main.java
@@ -7,38 +7,56 @@ import java.util.IntSummaryStatistics;
 public class Main {
 
     public static int Add(String numbers) {
+        StringBuffer nums = new StringBuffer(numbers);
         int sum = 0;
-        if(numbers.length() == 0) {}
-        else if (!numbers.contains(",") && !numbers.contains("/n")) {
-            sum = Integer.parseInt(numbers);
-        }
-        else if (numbers.contains(",") || numbers.contains(("/n"))) {
-            int index1 = 0;
-            StringBuffer nums = new StringBuffer(numbers);
-            while (nums.toString().contains(",") || nums.toString().contains("/n") || index1 == -1) {
-                int index = nums.toString().indexOf(',');
-                int nindex = nums.toString().indexOf('/');
-                if ((nindex < index || index == -1) && nindex != -1) {
-                    index = nindex;
-                    nums.deleteCharAt(index);
-                }
-
-                int num1 = Integer.parseInt(nums.substring(index1, index));
-                //int num2 = Integer.parseInt(numbers.substring(index + 1));
-                sum = sum + num1;
-                //System.out.print(index);
-                nums = nums.deleteCharAt(index);
-                index1 = index;
-                //System.out.print(nums.charAt(index));
+        String delimiter;
+        if (numbers.contains("//")) {
+            int index = nums.toString().indexOf('\\');
+            //System.out.print(index);
+            delimiter = numbers.substring(2,index);
+            nums.delete(0,index + 2);
+            //int index1 = 0;
+            while (nums.toString().contains(delimiter)) {
+                int findex = nums.indexOf(delimiter);
+                int lindex = findex + delimiter.length();
+                sum = sum + Integer.parseInt(nums.toString().substring(0,findex));
+                nums.delete(0,lindex);
+                //index1 = lindex;
+               // System.out.print(sum + " " + lindex);
             }
-            int num1 = Integer.parseInt(nums.substring(index1));
-            sum = sum + num1;
+            sum = sum + Integer.parseInt(nums.toString());
+        }
+        else {
+            if (numbers.length() == 0) {
+            } else if (!numbers.contains(",") && !numbers.contains("\\n")) {
+                sum = Integer.parseInt(numbers);
+            } else if (numbers.contains(",") || numbers.contains(("\\n"))) {
+                int index1 = 0;
+                while (nums.toString().contains(",") || nums.toString().contains("\\n") || index1 == -1) {
+                    int index = nums.toString().indexOf(',');
+                    int nindex = nums.toString().indexOf('\\');
+                    if ((nindex < index || index == -1) && nindex != -1) {
+                        index = nindex;
+                        nums.deleteCharAt(index);
+                    }
+
+                    int num1 = Integer.parseInt(nums.substring(index1, index));
+                    //int num2 = Integer.parseInt(numbers.substring(index + 1));
+                    sum = sum + num1;
+                    //System.out.print(index);
+                    nums = nums.deleteCharAt(index);
+                    index1 = index;
+                    //System.out.print(nums.charAt(index));
+                }
+                int num1 = Integer.parseInt(nums.substring(index1));
+                sum = sum + num1;
+            }
         }
         return sum;
     }
 
     public static void main(String[] args) {
-        String numbers = "1,25/n3,7/n5,4/n5";
+        String numbers = "//swanky\\n1swanky2swanky3swanky4";
         System.out.print(Add(numbers));
     }
 }
