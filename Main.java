b0cfb0cb7a99ec17edf4a491c9f81dbc8681864b
Kiddi <kiddinn95@gmail.com>
2015-10-16 02:10:02 +0000

    Skref 6

--- Main.java
+++ Main.java
@@ -25,8 +25,13 @@ public class Main {
                 int findex = nums.indexOf(delimiter);
                 int lindex = findex + delimiter.length();
                 int num = Integer.parseInt(nums.toString().substring(0,findex));
-                if (num < 0) {negatives = negatives + nums.toString().substring(0, findex);}
-                sum = sum + num;
+                if (num > 1000) {}
+                else {
+                    if (num < 0) {
+                        negatives = negatives + nums.toString().substring(0, findex);
+                    }
+                    sum = sum + num;
+                }
                 nums.delete(0,lindex);
             }
             if (Integer.parseInt(nums.toString()) < 0) {negatives = negatives + nums;}
@@ -38,8 +43,13 @@ public class Main {
         else {
             if (numbers.length() == 0) {}
             else if (!numbers.contains(",") && !numbers.contains("\\n")) {
-                if(Integer.parseInt(numbers) < 0) {throw new NegativeException(numbers);}
-                sum = Integer.parseInt(numbers);
+                if (Integer.parseInt(numbers) > 1000) {}
+                else {
+                    if (Integer.parseInt(numbers) < 0) {
+                        throw new NegativeException(numbers);
+                    }
+                    sum = Integer.parseInt(numbers);
+                }
             }
             else if (numbers.contains(",") || numbers.contains(("\\n"))) {
                 int index1 = 0;
@@ -51,22 +61,34 @@ public class Main {
                         nums.deleteCharAt(index);
                     }
                     int num1 = Integer.parseInt(nums.substring(index1, index));
-                    if (num1 < 0) { negatives = negatives + Integer.parseInt(nums.substring(index1, index)); }
-                    sum = sum + num1;
+                    if (num1 > 1000) {}
+                    else {
+                        if (num1 < 0) {
+                            negatives = negatives + Integer.parseInt(nums.substring(index1, index));
+                        }
+                        sum = sum + num1;
+                    }
                     nums = nums.deleteCharAt(index);
                     index1 = index;
                 }
                 int num1 = Integer.parseInt(nums.substring(index1));
-                if (num1 < 0) {negatives = negatives + Integer.parseInt(nums.substring(index1));}
-                if (!negatives.isEmpty()) { throw new NegativeException(negatives);}
-                sum = sum + num1;
+                if (num1 > 1000) {}
+                else {
+                    if (num1 < 0) {
+                        negatives = negatives + Integer.parseInt(nums.substring(index1));
+                    }
+                    if (!negatives.isEmpty()) {
+                        throw new NegativeException(negatives);
+                    }
+                    sum = sum + num1;
+                }
             }
         }
         return sum;
     }
 
     public static void main(String[] args) {
-        String numbers = "1,-2,3,-4\\n-5";
+        String numbers = "1001";
         System.out.print(Add(numbers));
     }
 }
