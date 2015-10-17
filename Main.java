144c56948a2c25e975acf7582077d2bb3f5f457c
Kiddi <kiddinn95@gmail.com>
2015-10-17 16:38:41 +0000

    Skref 8

--- Main.java
+++ Main.java
@@ -12,18 +12,54 @@ public class Main {
         }
     }
 
+    public static class delimBoolLength {
+        public boolean bool;
+        public String delim;
+        public delimBoolLength(boolean b, String l) {
+            bool = b;
+            delim = l;
+        }
+    }
+
+    public static delimBoolLength checkDelimiter(String string, String delimiter) {
+        StringBuffer delim = new StringBuffer(delimiter);
+        while (delim.toString().contains("]")) {
+            int index = delim.toString().indexOf(']');
+            try { String test = string.split("\\d")[1]; }
+            catch (ArrayIndexOutOfBoundsException aie) { return new delimBoolLength(false, null); }
+            if(string.split("\\d|-\\d")[1].contentEquals(delim.substring(0, index))) {
+            return new delimBoolLength(true, delim.toString().substring(0, index));
+            }
+            delim.delete(0, index + 2);
+        }
+        return new delimBoolLength(false, null);
+    }
+
     public static int Add(String numbers) throws NegativeException {
         StringBuffer nums = new StringBuffer(numbers);
         int sum = 0;
-        String delimiter;
+        String delimiter = ",][\\n]";
         String negatives = "";
-        if (numbers.contains("//")) {
+        if (numbers.contains("//[")) {
             int index = nums.toString().indexOf('\\');
-            delimiter = numbers.substring(3,index -1);
-            nums.delete(0,index + 2);
-            while (nums.toString().contains(delimiter)) {
-                int findex = nums.indexOf(delimiter);
-                int lindex = findex + delimiter.length();
+            delimiter = delimiter + numbers.substring(2, index);
+            nums.delete(0, index + 2);
+        }
+        if (numbers.length() == 0) {}
+        else {
+            if (!checkDelimiter(nums.toString(), delimiter).bool) {
+                if (Integer.parseInt(numbers) > 1000) {}
+                else {
+                   if (Integer.parseInt(numbers) < 0) {
+                       throw new NegativeException(numbers);
+                   }
+                    sum = Integer.parseInt(numbers);
+                }
+            }
+            while (checkDelimiter(nums.toString(), delimiter).bool) {
+                delimBoolLength dbl = checkDelimiter(nums.toString(), delimiter);
+                int findex = nums.indexOf(dbl.delim);
+                int lindex = findex + dbl.delim.length();
                 int num = Integer.parseInt(nums.toString().substring(0,findex));
                 if (num > 1000) {}
                 else {
@@ -40,55 +76,11 @@ public class Main {
             }
             sum = sum + Integer.parseInt(nums.toString());
         }
-        else {
-            if (numbers.length() == 0) {}
-            else if (!numbers.contains(",") && !numbers.contains("\\n")) {
-                if (Integer.parseInt(numbers) > 1000) {}
-                else {
-                    if (Integer.parseInt(numbers) < 0) {
-                        throw new NegativeException(numbers);
-                    }
-                    sum = Integer.parseInt(numbers);
-                }
-            }
-            else if (numbers.contains(",") || numbers.contains(("\\n"))) {
-                int index1 = 0;
-                while (nums.toString().contains(",") || nums.toString().contains("\\n") || index1 == -1) {
-                    int index = nums.toString().indexOf(',');
-                    int nindex = nums.toString().indexOf('\\');
-                    if ((nindex < index || index == -1) && nindex != -1) {
-                        index = nindex;
-                        nums.deleteCharAt(index);
-                    }
-                    int num1 = Integer.parseInt(nums.substring(index1, index));
-                    if (num1 > 1000) {}
-                    else {
-                        if (num1 < 0) {
-                            negatives = negatives + Integer.parseInt(nums.substring(index1, index));
-                        }
-                        sum = sum + num1;
-                    }
-                    nums = nums.deleteCharAt(index);
-                    index1 = index;
-                }
-                int num1 = Integer.parseInt(nums.substring(index1));
-                if (num1 > 1000) {}
-                else {
-                    if (num1 < 0) {
-                        negatives = negatives + Integer.parseInt(nums.substring(index1));
-                    }
-                    if (!negatives.isEmpty()) {
-                        throw new NegativeException(negatives);
-                    }
-                    sum = sum + num1;
-                }
-            }
-        }
         return sum;
     }
 
     public static void main(String[] args) {
-        String numbers = "//[swanky]\\n1swanky2swanky3";
+        String numbers = "//[swanky][cool]\\n1swanky2cool3swanky4cool5,6\\n7";
         System.out.print(Add(numbers));
     }
 }
